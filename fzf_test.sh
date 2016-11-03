43fcabb689068ff5042663359031fa13ed9c6bc9
Daron Chabot <chabot@frib.msu.edu>
2016-11-03 10:18:43 -0400

    ENH: delimit fzf search
    Only fuzzy find on <text> in grep output, <filename>:<linenum>:<text>.
    Also add '-H' and '--exclude-dir='.git' to grep switches.

--- fzf_test.sh
+++ fzf_test.sh
@@ -1,6 +1,7 @@
 fel () {
         local file lineno filename
-        file=$(grep -n --line-buffered --color=never -r "" * | fzf --query="$1" --select-1 --exit-0) 
+        file=$(grep -r -n -H --line-buffered --color=never --exclude-dir='.git' "" * | \
+               fzf --query="$1" --select-1 --exit-0 -d":" -n"3")
         lineno=$(echo "$file" | cut -d":" -f2)
         filename=$(echo "$file" | cut -d":" -f1)
         [ -n "$file" ] && ${EDITOR:-vim} +$lineno $filename
