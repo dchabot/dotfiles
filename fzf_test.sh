eb6b30ebf01d0290bc68b3a0c497c5560e3fbab7
Daron Chabot <chabot@frib.msu.edu>
2016-11-03 10:25:45 -0400

    REF: move fzf search fcn into bashrc

--- fzf_test.sh
+++
@@ -1,9 +0,0 @@
-# pilfered from here - https://github.com/neovim/neovim/issues/1670#issuecomment-67423125
-fel () {
-        local file lineno filename
-        file=$(grep -r -n -H --line-buffered --color=never --exclude-dir='.git' "" * | \
-               fzf --query="$1" --select-1 --exit-0 -d":" -n"3")
-        lineno=$(echo "$file" | cut -d":" -f2)
-        filename=$(echo "$file" | cut -d":" -f1)
-        [ -n "$file" ] && ${EDITOR:-vim} +$lineno $filename
-}
